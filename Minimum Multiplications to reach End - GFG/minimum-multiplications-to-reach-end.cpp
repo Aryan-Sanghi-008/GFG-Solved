8c5817f22c227ef2341b64410ae6889148f0087e
Aryan Sanghi <82806282+Aryan-Sanghi-008@users.noreply.github.com>
2023-09-07 04:04:13 +0530

    Added solution - LeetHub

--- Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.cpp
+++ Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.cpp
@@ -7,34 +7,39 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-class Solution
-{
-public:
+
+class Solution {
+  public:
     const int mod = 1e5;
-    int minimumMultiplications(vector<int> &arr, int start, int end)
-    {
-        vector<int> minn(1e5, 1e9);
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        int n = arr.size();
+        if(start == end)
+        {
+            return 0;
+        }
+        vector<int> minn(1e5 + 1, 1e9);
         queue<int> q;
-        q.push(start);
         int ops = 0;
-        while (!q.empty())
+        q.push(start);
+        while(!q.empty())
         {
             int si = q.size();
-            for (int i = 0; i < si; i++)
+            for(int i = 0; i < si; i++)
             {
-                int val= q.front();
+                int val = q.front();
                 q.pop();
-                if (val == end)
+                if(val == end)
                 {
                     return ops;
                 }
-                for (int j : arr)
+                for(auto j : arr)
                 {
-                    int newNum = ((val % mod) * (j % mod)) % mod;
-                    if ((1 + ops) < minn[newNum])
+                    int res = ((val % mod) * (j % mod)) % mod;
+                    
+                    if(ops + 1 < minn[res])
                     {
-                        q.push(newNum);
-                        minn[newNum] = 1 + ops;
+                        q.push(res);
+                        minn[res] = ops + 1;
                     }
                 }
             }
