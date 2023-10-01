e2e3a3afd0e89469f03ca35107af2f2313c22b7a
Aryan Sanghi <82806282+Aryan-Sanghi-008@users.noreply.github.com>
2023-10-02 01:23:17 +0530

    Added solution - LeetHub

--- Number of distinct subsequences - GFG/number-of-distinct-subsequences.cpp
+++ Number of distinct subsequences - GFG/number-of-distinct-subsequences.cpp
@@ -6,33 +6,30 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
-class Solution
-{
-public:
+class Solution{
+  public:
     const int mod = 1e9 + 7;
-    int distinctSubsequences(string s)
-    {
-        int n = s.length();
-        vector<int> dp(n + 1, 0);
-        int alph[26] = {-1};
-        dp[0] = 1;
-
-        for (int i = 0; i < n; i++)
-        {
-            if (alph[s[i] - 'a'] == -1)
-            {
-                dp[i + 1] = (dp[i] << 1);
-            }
-            else
-            {
-                dp[i + 1] = ((dp[i] << 1) % mod) - dp[alph[s[i] - 'a'] - 1] + mod;
-            }
-            alph[s[i] - 'a'] = i + 1;
-            dp[i + 1] %= mod;
-        }
-
-        return dp[n] % mod;
-    }
+	int distinctSubsequences(string s)
+	{
+	    int n = s.length();
+	    vector<int> alph(26, -1), dp(n + 1, -1);
+	    dp[0] = 1;
+	    for(int i = 0; i < n; i++)
+	    {
+	        if(alph[s[i] - 'a'] == -1)
+	        {
+	            dp[i + 1] = (dp[i] << 1);
+	        }
+	        else
+	        {
+	            dp[i + 1] = ((dp[i] << 1) % mod) - dp[alph[s[i] - 'a'] - 1] + mod;
+	        }
+	        alph[s[i] - 'a'] = i + 1;
+	        dp[i + 1] %= mod;
+	    }
+	    return dp[n] % mod;
+	    
+	}
 };
 
 //{ Driver Code Starts.
