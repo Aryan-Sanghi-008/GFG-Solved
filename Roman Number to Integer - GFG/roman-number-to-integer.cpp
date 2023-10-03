e45b4188ffd6e0a8e7804093aa5f5db541a2b7f1
Aryan Sanghi <82806282+Aryan-Sanghi-008@users.noreply.github.com>
2023-10-04 01:27:02 +0530

    Added solution - LeetHub

--- Roman Number to Integer - GFG/roman-number-to-integer.cpp
+++ Roman Number to Integer - GFG/roman-number-to-integer.cpp
@@ -10,13 +10,13 @@ using namespace std;
 
 class Solution {
   public:
-    int romanToDecimal(string &s)
+    int romanToDecimal(string &s) 
     {
-        int n = s.length(), ans = 0;
         map<char, int> mp = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-        for (int i = 0; i < n; i++)
+        int ans = 0, n = s.length();
+        for(int i = 0; i < n; i++)
         {
-            if ((i + 1) < n && mp[s[i]] < mp[s[i + 1]])
+            if(mp[s[i]] < mp[s[i + 1]])
             {
                 ans += mp[s[i + 1]] - mp[s[i]];
                 i++;
